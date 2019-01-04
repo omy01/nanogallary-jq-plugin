277ee18b72bca258ce639110f9eaf6ca6f7e769a
kamrul Alam Omy <42915567+omy01@users.noreply.github.com>
2019-01-04 15:31:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// jQuery(document).ready(function () {
+//   jQuery("#my_nanogallery2").nanogallery2({
+//   	items:[
+//     	{
+// 					src:   'berlin1.jpg',		        // image url
+// 					srct:  'berlin1t.jpg',	        // thumbnail url
+// 					title: 'Berlin 1',              // title
+//           description: 'this is Berlin'   // desceription
+// 			},
+// 		],
+//     thumbnailWidth:  'auto',
+//   	thumbnailHeight: 200,
+//     itemsBaseURL:    'https://nanogallery2.nanostudio.org/samples/',
+    
+// 		locationHash:    false
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#my_nanogallery2").nanogallery2({
+//   <!-- ### gallery settings ### -->
+//   thumbnailHeight:  'auto',
+//   thumbnailWidth:   'auto',
+//   itemsBaseURL:     'http://nanogallery2.nanostudio.org/samples/',
+// });
+
+
+// jQuery("#nanogallery2").nanogallery2({
+//   kind:             'flickr',
+//   userID:           '149035168@N08',
+//   thumbnailHeight:  150,
+//   thumbnailWidth:   150
+// });
+//     
